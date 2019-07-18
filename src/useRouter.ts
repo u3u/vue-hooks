8483d4391afdfddefd96256f71bdec93dad8664e
u3u <qwq@qwq.cat>
2019-07-18 09:46:16 +0800

    fix(hooks): `vm.$router` should not be computed wrapper

--- src/useRouter.ts
+++ src/useRouter.ts
@@ -4,6 +4,5 @@ import { getRuntimeVM } from './util/runtime';
 export default function useRouter() {
   const vm = getRuntimeVM();
   const route = computed(() => vm.$route);
-  const router = computed(() => vm.$router);
-  return { route, router };
+  return { route, router: vm.$router };
 }
