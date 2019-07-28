267afa4e7b6c8611756fd2eee9d8a177faa59dc4
u3u <qwq@qwq.cat>
2019-07-28 16:30:42 +0800

    docs: add storybook of `useStore`

---
+++ docs/useStore.md
@@ -0,0 +1,48 @@
+# useStore
+
+> You need to [use a plugin](https://github.com/u3u/vue-hooks#usage) before using this hook.
+
+Vue hook for [vuex](https://vuex.vuejs.org).
+
+## Usage
+
+```jsx {6,12,16}
+import { createComponent, computed } from 'vue-function-api';
+import { useStore } from '@u3u/vue-hooks';
+
+const Demo = createComponent({
+  setup() {
+    const store = useStore();
+    const plusOne = computed(() => store.value.state.count + 1);
+
+    const increment = () => store.value.commit('increment');
+    const incrementAsync = () => store.value.dispatch('incrementAsync');
+
+    return { store, plusOne, increment, incrementAsync };
+  },
+
+  render() {
+    const { store, plusOne } = this;
+    return (
+      <div>
+        <div>count: {store.state.count}</div>
+        <div>plusOne: {plusOne}</div>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.incrementAsync}>Increment Async</button>
+      </div>
+    );
+  },
+});
+```
+
+## Reference
+
+```typescript
+function useStore<TState>(): Wrapper<Store<TState>>;
+```
+
+### `ReturnValue`
+
+- [`Wrapper<Store<TState>>`](https://vuex.vuejs.org/api/#vuex-store-instance-properties)
+
+_[`TState`](https://www.typescriptlang.org/docs/handbook/generics.html) is used to specify the type of `store.state`_
---
+++ src/__stories__/useStore.story.tsx
@@ -0,0 +1,50 @@
+/* eslint import/no-extraneous-dependencies: off */
+import 'vue-tsx-support/enable-check';
+import Vue from 'vue';
+import { Store } from 'vuex';
+import { storiesOf } from '@storybook/vue';
+import { createComponent, computed } from 'vue-function-api';
+import { useStore } from '..';
+import { ShowDocs } from './components';
+import { createStore } from '../mocks';
+import { CounterState } from '../mocks/store';
+
+type Inject = {
+  store: Store<CounterState>;
+  plusOne: number;
+  increment: () => void;
+  incrementAsync: () => void;
+};
+
+const Docs = () => <ShowDocs md={require('../../docs/useStore.md')} />;
+
+const Demo = createComponent({
+  store: createStore(),
+
+  setup() {
+    const store = useStore<CounterState>();
+    const plusOne = computed(() => store.value.state.count + 1);
+
+    const increment = () => store.value.commit('increment');
+    const incrementAsync = () => store.value.dispatch('incrementAsync');
+
+    return { store, plusOne, increment, incrementAsync };
+  },
+
+  render(this: Vue & Inject) {
+    const { store, plusOne } = this;
+    return (
+      <div>
+        <div>count: {store.state.count}</div>
+        <div>plusOne: {plusOne}</div>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.incrementAsync}>Increment Async</button>
+      </div>
+    );
+  },
+});
+
+storiesOf('useStore', module)
+  // @ts-ignore
+  .add('docs', () => Docs)
+  .add('demo', () => Demo);
