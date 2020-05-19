6a37da2e2952a991567498884aa503a561a06bad
Han <xsytby1112@gmail.com>
2020-05-19 09:43:44 +0800

    feat: add a simple story demo & docs

---
+++ docs/useAsync.md
@@ -0,0 +1,46 @@
+# useAsync
+
+Vue hook for handling async state such as loading, success, error
+
+# Usage
+
+```jsx {9,10,14,17,30}
+import { createComponent } from '@vue/composition-api';
+import { useAsync } from '@u3u/vue-hooks';
+// async function like ajax etc
+import { getSomething } from '@vue/services';
+
+const Demo = createComponent({
+  setup() {
+    const { loading, resp, error, run } = useAsync(getSomething, {
+      manual: true,
+      params: [pass, some, arguments, to, asyncFunction],
+    });
+
+    const buttonText = computed(() => {
+      if (loading) {
+        return 'loading...'
+      }
+      if (resp.isNotEmpty) {
+        return 'success'
+      }
+      return Fetch
+    })
+
+    return { loading, resp, error, run, buttonText };
+  },
+
+  render() {
+    const { loading, resp, error, run, buttonText } = this;
+    return (
+      <div>
+        {if (!error) {
+          <button onClick="run">buttonText</button>
+        } else {
+          <span>Oops! Fetch Error</span>
+        }}
+      </div>
+    );
+  },
+});
+```
---
+++ src/__stories__/useAsync.story.tsx
@@ -0,0 +1,58 @@
+/* eslint import/no-extraneous-dependencies: off */
+import 'vue-tsx-support/enable-check';
+import Vue from 'vue';
+import { storiesOf } from '@storybook/vue';
+import { createComponent } from '@vue/composition-api';
+import { useAsync } from '..';
+import { ShowDocs } from './components';
+
+type Inject = {
+  loading: boolean;
+  error: Error | null;
+  resp: any;
+  run: () => Promise<any>;
+};
+
+const sleep = (ms = 0) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('success');
+    }, ms);
+  });
+
+const Docs = () => <ShowDocs md={require('../../docs/useAsync.md')} />;
+
+const Demo = createComponent({
+  setup() {
+    const { run, loading, resp, error } = useAsync(sleep, {
+      manual: true,
+      params: [2000],
+    });
+
+    return {
+      run,
+      loading,
+      resp,
+      error,
+    };
+  },
+
+  render(this: Vue & Inject) {
+    const { run, loading, resp, error } = this;
+    return (
+      <div>
+        <div>loading state: {loading.toString()}</div>
+        <div>resp state: {resp}</div>
+        <div style={{ marginTop: '10px' }}>error state: {error}</div>
+        <button onClick={() => run()}>
+          {loading ? 'loading...' : 'Click to fetch something'}
+        </button>
+      </div>
+    );
+  },
+});
+
+storiesOf('useAsync', module)
+  // @ts-ignore
+  .add('docs', () => Docs)
+  .add('demo', () => Demo);
